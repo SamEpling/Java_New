4ec309e177a2539608ce061f200e7c327fde4a55
Sam Epling <sgepling@icloud.com>
2023-09-12 14:59:09 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int age = 19;
+        int age = 18;
         String greeting = "Hello Joe, watta ya know?";
 
         System.out.println(greeting);
